dda6576d172c964cc9e06c47a952c6e6dc750023
lilbroadam <lilbroadam@gmail.com>
2020-11-28 16:01:00 -0600

    Print list of user's lights

--- lifxcmdln.py
+++ lifxcmdln.py
@@ -1,8 +1,21 @@
 import requests
 
-tokenfile = open("lifxtoken.txt", "r")
-token = tokenfile.readline()
+def print_light_names(lightsJson):
+    LIGHT_NAME = 'label'
 
-response = requests.get('https://api.lifx.com/v1/lights/all', auth=(token, ''))
+    print('Your lights:')
+    for i in range(len(lightsJson)):
+        print(i, ': ', lightsJson[i].get(LIGHT_NAME), sep="")
 
-print(response)
+def main():
+    tokenfile = open("lifxtoken.txt", "r")
+    token = tokenfile.readline()
+
+    response = requests.get('https://api.lifx.com/v1/lights/all', auth=(token, ''))
+
+    light_list = response.json()
+
+    print_light_names(light_list)
+
+if __name__=="__main__":
+    main()
