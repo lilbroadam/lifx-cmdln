c52beff6315e3b8ec2de96e54b2c37460e1abfe4
lilbroadam <lilbroadam@gmail.com>
2020-11-28 16:44:06 -0600

    Make power a command line argument
    "-power on" or "-power off"

--- lifxcmdln.py
+++ lifxcmdln.py
@@ -1,4 +1,7 @@
 import requests
+import sys, getopt
+
+args = {}
 
 def print_light_names(lightsJson):
     LIGHT_NAME = 'label'
@@ -6,6 +9,13 @@ def print_light_names(lightsJson):
     print('Your lights:')
     for i in range(len(lightsJson)):
         print(i, ': ', lightsJson[i].get(LIGHT_NAME), sep="")
+
+def build_payload():
+    payload = {}
+    for i in args.keys():
+        payload[i] = args.get(i)
+    
+    return payload
 
 def main():
     tokenfile = open("lifxtoken.txt", "r")
@@ -17,11 +27,22 @@ def main():
 
     print_light_names(light_list)
 
-    payload = {
-        "power": "on",
-    }
+    payload = build_payload()
 
     response = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload, auth=(token, ''))
 
 if __name__=="__main__":
+
+    argv = sys.argv[1:]
+    i = 0
+    while i < len(argv):
+        if argv[i] == '-power':
+            if argv[i + 1] != 'on' and argv[i + 1] != 'off':
+                print('Incorrect command line arguments')
+                sys.exit(2)
+            args['power'] = argv[i + 1]
+            i += 1
+        
+        i += 1
+
     main()
