098d9950a09690236219e8d206b98d18137c321c
lilbroadam <lilbroadam@gmail.com>
2020-11-29 18:40:33 -0600

    Code cleanup and add comments

--- lifxcmdln.py
+++ lifxcmdln.py
@@ -5,19 +5,23 @@ from lifxutil import *
 
 print_lights = False
 tokenfile = None
-args = {}
+cmdln_args = {}
 
 def main():
     token = read_token(tokenfile)
 
     if print_lights:
+        # Get a list of the user's lights from /lights/:all
         response = requests.get('https://api.lifx.com/v1/lights/all', auth=(token, ''))
         light_list = response.json()
         print_light_names(light_list)
 
-    payload = build_payload(args)
+    payload = build_payload(cmdln_args)
 
+    # Send the /state API request with the pararmeters from the command line
     response = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload, auth=(token, ''))
+
+    # Print the HTML status code if there was an error with the request
     status_code = response.status_code
     if status_code != 200 and status_code != 207:
         print('HTML request error', response.status_code)
@@ -30,21 +34,21 @@ if __name__=="__main__":
             if argv[i + 1] != 'on' and argv[i + 1] != 'off':
                 print('Incorrect command line arguments')
                 sys.exit(2)
-            args[POWER] = argv[i + 1]
             i += 1
+            cmdln_args[POWER] = argv[i]
         elif argv[i] == '-brightness' or argv[i] == '-b':
             # TODO validate brightness param is (0, 1.0) 
-            args[BRIGHTNESS] = argv[i + 1]
             i += 1
+            cmdln_args[BRIGHTNESS] = argv[i]
         elif argv[i] == '-kelvin' or argv[i] == '-k' or argv[i] == '-temperature' or argv[i] == '-t':
             # TODO validate kelvin param is (1500, 9000)
-            args[KELVIN] = argv[i + 1]
             i += 1
+            cmdln_args[KELVIN] = argv[i]
         elif argv[i] == '--list-lights':
             print_lights = True
         elif argv[i] == '--token-path':
-            tokenfile = open(argv[i + 1], 'r')
             i += 1
+            tokenfile = open(argv[i], 'r')
 
         i += 1
 
--- lifxutil.py
+++ lifxutil.py
@@ -1,28 +1,35 @@
+DEFAULT_TOKEN_FILE = 'lifxtoken.txt'
 LIGHT_NAME = 'label'
 POWER = 'power'
 BRIGHTNESS = 'brightness'
 COLOR = 'color'
 KELVIN = 'kelvin'
 
+# Read and return the token in tokenfile.
+# If tokenfile is None, read the token from DEFAULT_TOKEN_FILE
 def read_token(tokenfile):
     if tokenfile is None:
         try:
-            tokenfile = open("lifxtoken.txt", 'r')
+            tokenfile = open(DEFAULT_TOKEN_FILE, 'r')
         except FileNotFoundError:
             print('Error: unable to find token file.')
             exit(2)
     return tokenfile.readline()
 
+# Print the user's lights to the console
+# lightsJson is the JSON returned by the /lights API request
 def print_light_names(lightsJson):
     print('Your lights:')
     for i in range(len(lightsJson)):
         print(i, ': ', lightsJson[i].get(LIGHT_NAME), sep="")
 
+# Format the JSON payload for the /state api request
+# args is a dictionary of the state to be set (setting -> value)
 def build_payload(args):
     payload = {}
     for i in args.keys():
         if i == KELVIN:
-            color = 'kelvin:' + args.get(i)
+            color = KELVIN + ':' + args.get(i)
             payload[COLOR] = color
         else:
             payload[i] = args.get(i)
